94ca0f7a2358c85cff91251f389942102a7bc169
MelisaTaskin <melisatskknn@gmail.com>
2022-08-30 17:53:21 +0300

    30.08.2022

---
+++ Collections/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //arrayler yerıne kullanılır . 
+            //string[] isimler = new string[] {"engın","kerem","murat","halil" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+            //isimler = new string[5];
+            //isimler[4] = "İlker ";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+            List<string> isimler2 = new List<string> { "engın", "kerem", "murat", "halil" }; ;
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("ilker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+
+        }
+    }
+}
